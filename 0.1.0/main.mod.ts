7e6562e8f8edca4823ac4109d852b0459f256df3
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-05-13 12:07:06 +0200

    rename to polypacks

--- 0.1.0/main.mod.ts
+++ 0.1.0/main.mod.ts
@@ -2,12 +2,19 @@ import { PolyMod, PolyModLoader } from "https://pml.orangy.cfd/PolyTrackMods/Pol
 // IMPORTANT NOTE TO ME: COMMENT OUT BELOW LINE BEFORE PUSHING
 // import { PolyMod, PolyModLoader } from "../PolyModLoader/PolyModLoader";
 
-class ResourcePackMod extends PolyMod {
+class PolyPacks extends PolyMod {
     pml: PolyModLoader
+
+    savePacksToLocalStorage() {
+        let savedPacks: Array<{ base: string, loaded: boolean }> = [];
+        this.#packUrls = savedPacks;
+        this.localStorage.setItem("resourcepacks", JSON.stringify(this.#packUrls));
+    };
 
     init = (pmlInstance: PolyModLoader) => {
         this.pml = pmlInstance;
+        
     }
 }
 
-export const polyMod = new ResourcePackMod();
+export const polyMod = new PolyPacks();
